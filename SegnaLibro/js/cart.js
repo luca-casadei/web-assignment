869abfe525a38eecce1d1a68b9d61914dabcf242
Francesco Pazzaglia <francesco.pazzaglia2@gmail.com>
2025-01-04 17:00:36 +0100

    Edited cart api.
    Implemented cart api logic in order to remove an article and update the database.

--- SegnaLibro/js/cart.js
+++ SegnaLibro/js/cart.js
@@ -1,36 +1,45 @@
 function generateCartArticles(data) {
     let result = `
-    <h1>Carrello</h1>
-    <p>Totale: ${data.total_price}</p>
-    <input type="button" value="Procedi all'ordine" />
+        <h1>Carrello</h1>
     `;
 
     let articles = data.articles;
-    for (let i = 0; i < articles.length; i++) {
-        let article = `
-        <article>
-            <header>
-                <figure>
-                    <img src="${articles[i]["NomeImmagine"]}" alt="" />
-                </figure>
-                <h2>${articles[i]["TitoloAnnuncio"]}</h2>
-                <p>${articles[i]["NomeAutore"]} ${articles[i]["CognomeAutore"]}</p>
-                <input type="button" alt="Rimuovi articolo dal carrello" />
-            </header>
-            <p>${articles[i]["NomeCategoria"]}</p>
-            <p>${articles[i]["DescrizioneAnnuncio"]}</p>
-            <footer>
-                <p>${articles[i]["Prezzo"]}</p>
-                <p>Condizione: <span>${articles[i]["NomeCondizione"]}</span></p>
-            </footer>
-        </article>
-        `;
-        result += article;
+    if (articles.length !== 0) {
+        result += `<p>Totale: ${data.total_price}</p><input type="button" value="Procedi all\'ordine" />`;
+        for (let i = 0; i < articles.length; i++) {
+            let article = `
+            <article>
+                <header>
+                    <figure>
+                        <img src="${articles[i]["NomeImmagine"]}" alt="" />
+                    </figure>
+                    <h2>${articles[i]["TitoloAnnuncio"]}</h2>
+                    <p>${articles[i]["NomeAutore"]} ${articles[i]["CognomeAutore"]}</p>
+                    <input type="button" 
+                        alt="Rimuovi articolo dal carrello" 
+                        onclick="removeArticle('${articles[i]["NumeroCopia"]}', '${articles[i]["EAN"]}', '${articles[i]["CodiceRegGroup"]}', '${articles[i]["CodiceEditoriale"]}', '${articles[i]["CodiceTitolo"]}')"
+                    />
+                </header>
+                <p>ISBN: ${articles[i]["EAN"]}-${articles[i]["CodiceRegGroup"]}-${articles[i]["CodiceEditoriale"]}-${articles[i]["CodiceTitolo"]}-${articles[i]["CifraControllo"]}</p>
+                <p>${articles[i]["NomeCategoria"]}</p>
+                <p>${articles[i]["DescrizioneAnnuncio"]}</p>
+                <footer>
+                    <p>${articles[i]["Prezzo"]}</p>
+                    <p>Condizione: <span>${articles[i]["NomeCondizione"]}</span></p>
+                </footer>
+            </article>
+            `;
+            result += article;
+        }
+    } else {
+        result += "<p>Il carrello è vuoto</p>";
     }
+
     return result;
 }
 
 async function getCartArticlesData() {
+    console.log("getCartArticlesData");
     const url = './apis/api-cart.php';
     try {
         const response = await fetch(url);
@@ -38,12 +47,37 @@ async function getCartArticlesData() {
             throw new Error(`Response status: ${response.status}`);
         }
         const json = await response.json();
-        const articles = generateCartArticles(json);
-        document.querySelector("main").innerHTML = articles;
+        const data = generateCartArticles(json);
+        document.querySelector("main").innerHTML = data;
     } catch (error) {
         console.log(error.message);
     }
 }
 
-console.log("Cart page loaded");
+async function removeArticle(numero_copia, ean, codice_reg_group, codice_editoriale, codice_titolo) {
+    console.log(numero_copia, ean, codice_editoriale, codice_reg_group, codice_titolo);
+    const url = './apis/api-cart.php';
+    const data = new FormData();
+    data.append('numero_copia', numero_copia);
+    data.append('ean', ean);
+    data.append('codice_reg_group', codice_reg_group);
+    data.append('codice_editoriale', codice_editoriale);
+    data.append('codice_titolo', codice_titolo);
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            body: data
+        });
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const json = await response.json();
+        if (json.status === "success") {
+            getCartArticlesData();
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
 getCartArticlesData();
