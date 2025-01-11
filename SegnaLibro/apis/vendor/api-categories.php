b533ee7ccf5a733d4aa3824c212a9da210c11cf6
Luca Casadei <casadeiluca30@gmail.com>
2025-01-11 18:55:41 +0100

    Completed every function of advanced research

--- SegnaLibro/apis/vendor/api-categories.php
+++ SegnaLibro/apis/vendor/api-categories.php
@@ -1,10 +1,6 @@
 <?php
 require ('../../bootstrap.php');
-if (isUserLoggedIn() && isUserVendor()){
-    $categories = $dbh->getCategories();
-    header('Content-Type: application/json');
-    echo json_encode($categories);
-} else{
-    http_response_code("403");
-}
+$categories = $dbh->getCategories();
+header('Content-Type: application/json');
+echo json_encode($categories);
 ?>
--- SegnaLibro/pages/advancedsearch.php
+++ SegnaLibro/pages/advancedsearch.php
@@ -1,13 +1,22 @@
 <header>
-    <search><input type="search" placeholder="Cerca libro..." aria-label="Ricerca libro per titolo" oninput="onSearchChange()" id="contentsearch"/></search>
+    <search><input type="search" placeholder="Cerca libro..." aria-label="Ricerca libro per titolo" oninput="updateSearchTerms()" id="contentsearch"/></search>
     <form action="#" method="POST">
         <h2>Filtri</h2>
-        <select title="Categoria"></select>
-        <select title="Genere"></select>
-        <select title="Ordinamento"></select>
+        <select title="Categoria" id="categoryselect" onchange="updateSearchTerms()"></select>
+        <select title="Ordinamento" id="orderingselect" onchange="updateSearchTerms()">
+            <optgroup label="Prezzo">
+                <option value="pdesc">Da più a meno costoso</option>
+                <option value="pasc">Da meno a più costoso</option>
+            </optgroup>
+            <optgroup label="Titolo">
+                <option value="tdesc">Da A a Z</option>
+                <option value="tasc">Dalla Z alla A</option>
+            </optgroup>
+        </select>
         <div>
             <label for="pricerange">Prezzo:</label>
-            <input type="range" id="pricerange" name="pricerange"/>
+            <input type="range" min="5" max="100" step="5" onchange="updateSearchTerms()" id="pricerange" name="pricerange"/>
+            <p></p>
         </div>
     </form>
 </header>
