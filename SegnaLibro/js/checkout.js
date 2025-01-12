fc98835855ad338b6461658659115d8daca52f6d
Francesco Pazzaglia <francesco.pazzaglia2@gmail.com>
2025-01-12 18:13:14 +0100

    Merge branch 'master' of https://github.com/luca-casadei/web-assignment

--- SegnaLibro/js/checkout.js
+++ SegnaLibro/js/checkout.js
@@ -1,1 +1,5 @@
 console.log("Checkout page");
+
+function redirectToCompleteOrder() {
+  window.location.href = "complete_order_index.php";
+}
--- docs/database/copia.sql
+++ docs/database/copia.sql
@@ -4,8 +4,11 @@ INSERT INTO `segnalibro_logic`.`CONDIZIONE` (`Codice`, `Nome`) VALUES ('3', 'Med
 INSERT INTO `segnalibro_logic`.`CONDIZIONE` (`Codice`, `Nome`) VALUES ('4', 'Rovinato');
 INSERT INTO `segnalibro_logic`.`CONDIZIONE` (`Codice`, `Nome`) VALUES ('5', 'Illeggibile');
 
-INSERT INTO `segnalibro_logic`.`COPIA` (`EAN`, `CodiceRegGroup`, `CodiceEditoriale`, `CodiceTitolo`, `Numero`, `Prezzo`, `Titolo`, `Descrizione`, `DataAnnuncio`, `CodiceCondizione`) VALUES ('978', '12345', '001001', '000001', '1', '5.99', 'Il nome della Rosa, ottime condizioni!', 'Vendita de \"Il nome della Rosa\" in ottime condizioni, usato a basso prezzo!', '2024-12-31', '2');
+INSERT INTO `segnalibro_logic`.`COPIA` (`EAN`, `CodiceRegGroup`, `CodiceEditoriale`, `CodiceTitolo`, `Numero`, `Prezzo`, `Titolo`, `Descrizione`, `DataAnnuncio`, `CodiceCondizione`) VALUES 
+('978', '12345', '001001', '000001', '1', '5.99', 'Il nome della Rosa, ottime condizioni!', 'Vendita de \"Il nome della Rosa\" in ottime condizioni, usato a basso prezzo!', '2024-12-31', '2'),
+('978', '12345', '001001', '000002', '1', '10.99', 'Io Uccido messo così così', 'Vendita de \"Io Uccido\", non è messo troppo male dai, prendetelo vi prego.', '2025-01-09', '3');
 
 INSERT INTO `segnalibro_logic`.`IMMAGINE` (`Numero`, `Percorso`, `NumeroCopia`, `EAN`, `CodiceRegGroup`, `CodiceEditoriale`, `CodiceTitolo`) VALUES 
 ('2','978-12345-001001-000001-1-2.jpg', '1', '978', '12345', '001001', '000001'),
-('1','978-12345-001001-000001-1-1.jpg', '1', '978', '12345', '001001', '000001');
+('1','978-12345-001001-000001-1-1.jpg', '1', '978', '12345', '001001', '000001'),
+('3','978-12345-001001-000002-1-1.jpg', '1', '978', '12345', '001001', '000002');
