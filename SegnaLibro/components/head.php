317862c728ab64f453317ba60afdf5b4e30b3f05
Luca Casadei <casadeiluca30@gmail.com>
2025-01-15 01:51:50 +0100

    Added closing tag in head

--- SegnaLibro/components/head.php
+++ SegnaLibro/components/head.php
@@ -1,6 +1,6 @@
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
     <title><?php echo $tp["title"]; ?></title>
     <link rel="stylesheet" type="text/css" href="./css/style.css" />
 </head>
